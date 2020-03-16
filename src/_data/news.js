b755ac2b837951feef5c0c7d0eddfb929cbbb088
Jon Bos <bos.jon@gmail.com>
2020-03-16 12:57:47 -0500

    better env var name

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = async function() {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/top-headlines?q=trump&apiKey=${process.env.API_KEY}`
+      `http://newsapi.org/v2/top-headlines?q=trump&apiKey=${process.env.NEWS_API_KEY}`
     );
     return response.data;
   } catch (error) {
