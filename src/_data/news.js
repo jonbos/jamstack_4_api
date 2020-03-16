eb1c8067a9dfffe9c739e0e276090b2a9ff74496
Jon Bos <bos.jon@gmail.com>
2020-03-16 13:09:13 -0500

    make the articles less bleak

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = async function() {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/top-headlines?q=trump&apiKey=${process.env.NEWS_API_KEY}`
+      `http://newsapi.org/v2/everything?q=apple&from=2020-03-15&to=2020-03-15&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`
     );
     return response.data;
   } catch (error) {
